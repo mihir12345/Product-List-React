0c33cadae63974ddc27d8bc55b08a6fbec4bd4fd
Mihir Patil <mihirpatil81@gmail.com>
2021-09-13 09:47:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import SearchBar from './components/SearchBar.component';
+import ProductTable from './components/ProductTable.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      filterText:'',
+      inStockOnly:false
+    }
+  }
+handleFilterTextChange=(filterText)=>{
+  this.setState({
+    filterText: filterText
+  })
 }
+
+handleInStockChange=(inStockOnly)=>{
+  this.setState({
+    inStockOnly:inStockOnly
+  })
+}
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+        filterText={this.state.filterText}
+        inStockOnly={this.state.inStockOnly}
+        onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}/>
+
+        <ProductTable products={this.props.products}
+        filterText={this.state.filterText}
+        inStockOnly={this.state.inStockOnly}/>
+      </div>
+    );
+  }
+}
+
+// const PRODUCTS = [
+//   {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+//   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+//   {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+//   {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+//   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+//   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+// ];
 
 export default App;
---
+++ src/components/SearchBar.component.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    // constructor(props){
+    //     super(props)
+
+    // }
+
+    handleFilterTextChange=(e)=>{
+        this.props.onFilterTextChange(e.target.value)
+    }
+
+    handleInStockChange=(e)=>{
+        this.props.onInStockChange(e.target.checked)
+    }
+
+    render() {
+        const filterText=this.props.filterText;
+        const inStockOnly=this.props.inStockOnly;
+
+        return (
+            <form>
+                <input type='text' placeholder='search...' value={this.props.filterText} onChange={this.handleFilterTextChange}/>
+                <p> 
+                    <input type='checkbox' checked={this.props.inStockOnly}   onChange={this.handleInStockChange}/>
+                    {''} only show products in stock
+                </p>
+            </form>
+        );
+    }
+}
+
+export default SearchBar;
